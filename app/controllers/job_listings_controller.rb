113169f8a205b08cf96e9d863548a43e04db4863
Brandon Falter <brandon@voie.com>
2020-03-11 18:16:32 +0800

    added user and agent dashboards and routes with links on pages

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -1,9 +1,25 @@
 class JobListingsController < ApplicationController
+
+  def index
+    @job_listings = JobListing.all
+  end
+
   def new
     @job_listing = JobListing.new
   end
 
   def create
+    @job_listing = JobListing.new(job_listing_params)
+    if @job_listing.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def job_listing_params
+    params.require(:job_listing).permit(:title, :description, :address, photos: [], :category_ids)
   end
 end
