0aa0222901f46943337bc047d8a1fd689369477c
Shafiur Ibrahim <shafiuribrahim@gmail.com>
2020-03-12 11:30:02 +0800

    added ifelse statemnt on the navbar, added missing validations

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -6,4 +6,9 @@ class Agent < ApplicationRecord
   has_many :reviews, through: :offers
   has_many :agent_professions
   has_many :professions, through: :agent_professions
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :company_name, presence: true
+  validates :company_address, presence: true
 end
